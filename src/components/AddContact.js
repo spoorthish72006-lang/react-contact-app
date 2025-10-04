7f19d91a209ada41b2f34121edaed06df5b047e6
dusklight00 <r.rahul.developer@gmail.com>
2025-10-05 03:45:41 +0530

    Refactor AddContact component and update App component to pass contacts and setContact props

--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -1,53 +1,62 @@
 import react from "react";
 
-class AddContact extends react.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-        name:"",
-        email:"",
-    }
+class AddContact extends react.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      email: "",
+    };
 
-}
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-    render(){
-        return (
-            <div className="ui main">
-                <h2>Add Contact</h2>
-                <form className="ui form">
-                    <div className="field">
-                        <label>Name</label>
-                        <input 
-                        types="text"
-                        name="name"
-                        placeholder="Name"
-                        value={this.state.name}
-                        onChange={ (e) => this.setState({name: e.target.value})}
-                    />
-                    </div>
-                    <div className="field">
-                        <label>Email</label>
-                        <input 
-                        types="text"
-                        name="email"
-                        placeholder="Email"
-                        value={this.state.email}
-                        onChange={ (e) => this.setState({email: e.target.value})}
-                        />
-                    </div>
-                    <button className="ui button blue" onClick={(e)=>{
-                        e.preventDefault();
-                        this.props.setContact([...this.props.contacts,{
-                        id:"123",
-                        name:this.state.name,
-                        email:this.state.email
-                    }])}}>Add</button>
+  handleSubmit(event) {
+    event.preventDefault();
+    this.props.setContact([
+      ...this.props.contacts,
+      {
+        id: "123",
+        name: this.state.name,
+        email: this.state.email,
+      },
+    ]);
 
-                </form>
+    this.setState({ name: "", email: "" });
+  }
 
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="ui main">
+        <h2>Add Contact</h2>
+        <form className="ui form">
+          <div className="field">
+            <label>Name</label>
+            <input
+              types="text"
+              name="name"
+              placeholder="Name"
+              value={this.state.name}
+              onChange={(e) => this.setState({ name: e.target.value })}
+            />
+          </div>
+          <div className="field">
+            <label>Email</label>
+            <input
+              types="text"
+              name="email"
+              placeholder="Email"
+              value={this.state.email}
+              onChange={(e) => this.setState({ email: e.target.value })}
+            />
+          </div>
+          <button className="ui button blue" onClick={this.handleSubmit}>
+            Add
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default AddContact;
--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,24 @@
- import {useState}from "react";
+import { useState } from "react";
 
 import Header from "./Header";
 import AddContact from "./AddContact";
-import ContactList from "./ContactList"
+import ContactList from "./ContactList";
 
 function App() {
- const[ contacts, setContact] = useState([{
-  id:"user39",
-  name:"sanvi",
-  email:"sanvi@gmail.com"
- }])
-   
+  const [contacts, setContact] = useState([
+    {
+      id: "user39",
+      name: "sanvi",
+      email: "sanvi@gmail.com",
+    },
+  ]);
+
   return (
     <div className="ui container">
       <Header />
-      <AddContact />
-      <ContactList contacts={contacts} setContact={setContact}/> 
+      <AddContact contacts={contacts} setContact={setContact} />
+      <ContactList contacts={contacts} />
     </div>
   );
 }
 export default App;
-
